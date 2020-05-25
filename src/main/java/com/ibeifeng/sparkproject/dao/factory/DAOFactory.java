c09730aeadc9f758e961c316ecb4e39462be9cc7
hxd <982304089@qq.com>
2020-05-25 21:18:22 +0800

    new file:   src/main/java/com/ibeifeng/sparkproject/dao/ISessionAggrStatDAO.java 	new file:   src/main/java/com/ibeifeng/sparkproject/dao/daoimpl/SessionAggrStatDAOImpl.java 	modified:   src/main/java/com/ibeifeng/sparkproject/dao/factory/DAOFactory.java 	modified:   src/main/java/com/ibeifeng/sparkproject/spark/session/UserVisitSessionAnalyzeSpark.java 	.idea/ 	sparkproject.iml 	target/

--- src/main/java/com/ibeifeng/sparkproject/dao/factory/DAOFactory.java
+++ src/main/java/com/ibeifeng/sparkproject/dao/factory/DAOFactory.java
@@ -7,11 +7,16 @@
 
 package com.ibeifeng.sparkproject.dao.factory;
 
+import com.ibeifeng.sparkproject.dao.ISessionAggrStatDAO;
 import com.ibeifeng.sparkproject.dao.ITaskDAO;
+import com.ibeifeng.sparkproject.dao.daoimpl.SessionAggrStatDAOImpl;
 import com.ibeifeng.sparkproject.dao.daoimpl.TaskDAOImpl;
 
 public class DAOFactory {
     public static ITaskDAO getTaskDAO() {
         return new TaskDAOImpl();
+    }
+    public static ISessionAggrStatDAO getSessionAggrStatDAO(){
+        return new SessionAggrStatDAOImpl();
     }
 }
